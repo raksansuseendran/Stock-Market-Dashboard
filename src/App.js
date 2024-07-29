eeeadeba7c3b5e7bb565224e278a0be1b15ed9b7
Raksan <98756460+raksansuseendran@users.noreply.github.com>
2024-07-30 03:18:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// App.js
+
+import React from 'react';
+import Header from './Components/Header';
+import StockChart from './Components/StockChart';
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<StockChart />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+
+import React from 'react';
+
+function Header() {
+	return (
+		<center>
+			<h1>Stock Market Prediction App</h1>
+		</center>
+	);
+}
+
+export default Header;
